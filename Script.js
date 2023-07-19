1d3705b85217f5d04c1bcac7b7606d24dd03b74f
Prasannarajpaul <104220676+Prasannarajpaul@users.noreply.github.com>
2023-07-19 22:48:05 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,11 +1,11 @@
 const imgUrls=[
     "https://images.squarespace-cdn.com/content/v1/616cfb6a0343cf58c0a570b5/a1b29bc4-b049-4b06-acf7-f86ca6052be9/XLR8.png",
-    "https://i.pinimg.com/originals/50/cc/cb/50cccb535ffc1550555ac33a87b632b3.png",
     "http://4.bp.blogspot.com/-YPiiMZn8gnc/TkZiGTN62cI/AAAAAAAABkI/rG9YKXE8hno/s1600/Ben-10-upgrade-wallpaper.png",
+    "https://e1.pxfuel.com/desktop-wallpaper/985/536/desktop-wallpaper-diamondhead-gallery-ben-10-diamondhead.jpg",
     "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/04fd096e-205c-4c04-bdde-7ce9bc718319/de3soc8-47c86a94-8b94-48e3-aa03-69db82d498d3.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzA0ZmQwOTZlLTIwNWMtNGMwNC1iZGRlLTdjZTliYzcxODMxOVwvZGUzc29jOC00N2M4NmE5NC04Yjk0LTQ4ZTMtYWEwMy02OWRiODJkNDk4ZDMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.uctpM_z1HOitkZGPHby-tFD9YomFRMJYk1gJQQ1ZKwA",
     "https://i.pinimg.com/originals/fb/b5/9d/fbb59d045b39305d1b363863d1e131e5.png",
     "https://w7.pngwing.com/pngs/694/821/png-transparent-four-arms-ben-10-omniverse-alien-ben-fictional-character-supernatural-creature-red-thumbnail.png",
-    "https://assets.stickpng.com/images/5a944b179fc609199d0fef19.png",
+    "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c05cec11-03a8-473e-8a56-672f2054ca80/dery6q2-ee7310c9-2818-455a-a7ed-9a2690f80297.png/v1/fill/w_695,h_1149,strp/albedo_greymatter_real_form_by_ultraben10man_dery6q2-pre.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTE1NSIsInBhdGgiOiJcL2ZcL2MwNWNlYzExLTAzYTgtNDczZS04YTU2LTY3MmYyMDU0Y2E4MFwvZGVyeTZxMi1lZTczMTBjOS0yODE4LTQ1NWEtYTdlZC05YTI2OTBmODAyOTcucG5nIiwid2lkdGgiOiI8PTY5OSJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.WgnWgpxMrJKrCr9TD6O6SO9tSxDcrNjMrSIIzB1Vgz0",
     "https://i.pinimg.com/originals/4d/cd/55/4dcd55bdcf3a50cbfc30b944c71d0f6f.png",
     "https://i.redd.it/wl7czf7tnpe71.png",
     "https://www.pngitem.com/pimgs/m/522-5227896_ripjaws-ben-10-aliens-hd-png-download.png"
