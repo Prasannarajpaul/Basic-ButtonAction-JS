83eb4a75c91804227261a90b2108868bc9f225ca
Prasannarajpaul <104220676+Prasannarajpaul@users.noreply.github.com>
2023-07-19 22:45:13 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,7 +1,7 @@
 const imgUrls=[
     "https://images.squarespace-cdn.com/content/v1/616cfb6a0343cf58c0a570b5/a1b29bc4-b049-4b06-acf7-f86ca6052be9/XLR8.png",
     "https://i.pinimg.com/originals/50/cc/cb/50cccb535ffc1550555ac33a87b632b3.png",
-    "https://e1.pxfuel.com/desktop-wallpaper/985/536/desktop-wallpaper-diamondhead-gallery-ben-10-diamondhead.jpg",
+    "http://4.bp.blogspot.com/-YPiiMZn8gnc/TkZiGTN62cI/AAAAAAAABkI/rG9YKXE8hno/s1600/Ben-10-upgrade-wallpaper.png",
     "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/04fd096e-205c-4c04-bdde-7ce9bc718319/de3soc8-47c86a94-8b94-48e3-aa03-69db82d498d3.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzA0ZmQwOTZlLTIwNWMtNGMwNC1iZGRlLTdjZTliYzcxODMxOVwvZGUzc29jOC00N2M4NmE5NC04Yjk0LTQ4ZTMtYWEwMy02OWRiODJkNDk4ZDMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.uctpM_z1HOitkZGPHby-tFD9YomFRMJYk1gJQQ1ZKwA",
     "https://i.pinimg.com/originals/fb/b5/9d/fbb59d045b39305d1b363863d1e131e5.png",
     "https://w7.pngwing.com/pngs/694/821/png-transparent-four-arms-ben-10-omniverse-alien-ben-fictional-character-supernatural-creature-red-thumbnail.png",
